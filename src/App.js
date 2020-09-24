2551160136e4e63b3397806023c0f8ce66a6a51a
karen <karen.chilingaryann98@gmail.com>
2020-09-25 03:32:50 +0400

    create component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Header() {
+  return (
+      <header className="App-header">
+        <span className="header-page">Home</span>
+        <span className="header-page">Country</span>
+        <span className="header-page">City</span>
+        <span className="header-page">Contact</span>
+        <span className="header-page">About</span>
+      </header>
+  )
+}
+
+function Button() {
+  return (
+      <button className="button">BUTTON</button>
+  );
+}
+
+const Image = () => (
+    <div className="image">
+      <img src="https://i.ytimg.com/vi/6NVCKeiqvtY/maxresdefault.jpg" alt=""/>
+      <img src="https://i.ytimg.com/vi/d143Bd0howo/maxresdefault.jpg" alt=""/>
+      <img src="https://ak7.picdn.net/shutterstock/videos/22336627/thumb/1.jpg" alt=""/>
+      <img src="https://i.ytimg.com/vi/x30YOmfeVTE/maxresdefault.jpg" alt=""/>
+      <img src="https://i.ytimg.com/vi/q64jlLL8hIg/maxresdefault.jpg" alt=""/>
+      <img src="https://i.ytimg.com/vi/6NVCKeiqvtY/maxresdefault.jpg" alt=""/>
+      <img src="https://i.ytimg.com/vi/d143Bd0howo/maxresdefault.jpg" alt=""/>
+      <img src="https://ak7.picdn.net/shutterstock/videos/22336627/thumb/1.jpg" alt=""/>
+      <img src="https://i.ytimg.com/vi/x30YOmfeVTE/maxresdefault.jpg" alt=""/>
+      <img src="https://i.ytimg.com/vi/q64jlLL8hIg/maxresdefault.jpg" alt=""/>
+    </div>
+)
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="container">
+        <Image />
+        <Button />
+      </div>
     </div>
   );
 }
