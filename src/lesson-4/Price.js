39943db1c511dc9e0eea9ea78a3c5a8646c0542c
karen <karen.chilingaryann98@gmail.com>
2020-09-30 09:20:59 +0400

    lesson-5

--- src/lesson-4/Price.js
+++ src/lesson-4/Price.js
@@ -3,10 +3,34 @@ import React, {Component} from 'react';
 class Price extends Component{
     constructor (props){
         super(props)
+        this.state = {
+            price: props.price
+        }
+    }
+
+    changePrice = () => {
+        let count = this.state.price;
+        if (this.state.price.search("֏") <= 0){
+            this.setState({
+                price: count.slice(0, count.length-1) * 487 + "֏"
+            })
+        }else {
+            this.setState({
+                price: count.slice(0, count.length-1)/487 + "$"
+            })
+        }
+
     }
     render() {
         return (
-            <div className="params">{this.props.price}</div>
+            <div className="params">
+                {this.state.price}
+                <button
+                    className="changePrice"
+                    onClick={this.changePrice}
+                >Change the currency
+                </button>
+            </div>
         )
     }
 }
